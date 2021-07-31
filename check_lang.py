6603ea870402ddea0f2ebbc4ef4b7851f864c48a
renol767 <72129061+renol767@users.noreply.github.com>
2021-07-31 14:58:52 +0700

    initial_commit

---
+++ check_lang.py
@@ -0,0 +1,12 @@
+# Untuk Mengecek Language/Bahasa apa saja yang tersedia
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+for voice in voices:
+    print("Voice: %s" % voice.name)
+    print(" - ID: %s" % voice.id)
+    print(" - Languages: %s" % voice.languages)
+    print(" - Gender: %s" % voice.gender)
+    print(" - Age: %s" % voice.age)
+    print("\n")
